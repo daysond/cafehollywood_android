64a7e7aa8a4cf5e3948c4b842f06edcc12157531
Dayson Dong <daysond@outlook.com>
2020-11-25 04:54:26 -0500

    change meal list page to sliver app bar

--- lib/screens/menu/meal_list_page.dart
+++ lib/screens/menu/meal_list_page.dart
@@ -14,15 +14,62 @@ class MealListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: null,
-        leading: CupertinoNavigationBarBackButton(
-          color: Colors.black,
-          onPressed: () => Navigator.pop(context),
-        ),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('Menu A'),
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: false,
+              // title: Text('This is my menu'),
+              background: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350"),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(32, 32, 32, 32),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'title',
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          'subtitle',
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white),
+                        ),
+                      ]),
+                ),
+              ),
+            ),
+            expandedHeight: 280,
+          ),
+        ],
       ),
-      child: MealList(),
     );
+
+    // CupertinoPageScaffold(
+    //   navigationBar: CupertinoNavigationBar(
+    //     backgroundColor: null,
+    //     leading: CupertinoNavigationBarBackButton(
+    //       color: Colors.black,
+    //       onPressed: () => Navigator.pop(context),
+    //     ),
+    //   ),
+    //   child: MealList(),
+    // );
   }
 }
